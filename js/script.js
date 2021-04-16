0ab1db327178310b49de1bebcf53a8f7b36774ff
Anowar57 <82600282+Anowar57@users.noreply.github.com>
2021-04-16 10:19:57 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+  // Name typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+
+// ================ animated
+AOS.init({
+  duration: 1200,
+});
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+ 
+
+
+// ===================== Movile Nav Bar
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
